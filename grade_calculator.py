d6e06538e88cea776689c921618258d97a56d505
shifaalzhurur <shifaalzhurur@gmail.com>
2026-03-22 15:45:50 +0530

    Update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,14 +1,20 @@
-# Student Grade Calculator
+# Student Grade Calculator Program
 
+# Taking student name as input
 name = input("Enter student name: ")
 
+# Taking marks for 3 subjects
 m1 = int(input("Enter marks for Subject 1: "))
 m2 = int(input("Enter marks for Subject 2: "))
 m3 = int(input("Enter marks for Subject 3: "))
 
+# Calculating total marks
 total = m1 + m2 + m3
+
+# Calculating average
 average = total / 3
 
+# Determining grade using if-elif-else
 if average >= 90:
     grade = 'A'
 elif average >= 75:
@@ -18,8 +24,9 @@ elif average >= 50:
 else:
     grade = 'Fail'
 
+# Displaying the result
 print("\n----- RESULT -----")
 print("Name:", name)
-print("Total:", total)
+print("Total Marks:", total)
 print("Average:", average)
 print("Grade:", grade)
